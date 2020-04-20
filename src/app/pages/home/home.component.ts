84d52bfcd2db9cc90b2bd3a17e135f32236cf010
soniapini <me@soniapini.info>
2020-04-20 18:02:31 +0200

    feat(home): aggiunta descrizione prima pagina base table

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  baseTableDemoDescription: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.baseTableDemoDescription = `Demo Base Table con primi editor custom. <br />
+    Campo Sport: utilizza editor custom per soli caratteri alfabetici. <br />
+    Campi Age e Year: utilizzano editor custom per soli caratteri numerici. `;
+
   }
 
 }
