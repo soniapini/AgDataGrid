7e22066ab53fa34ef7953335bf21480747e7e48a
Francesco Giunta <francesco.giunta@nispro.it>
2020-04-23 18:05:21 +0200

    fix css libreria

--- projects/se-ui-datagrid/src/lib/se-ui-datagrid.component.ts
+++ projects/se-ui-datagrid/src/lib/se-ui-datagrid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'lib-se-ui-datagrid',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
       se-ui-datagrid works!
     </p>
   `,
-  styles: []
+  styleUrls: ['./se-ui-datagrid.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SeUiDatagridComponent implements OnInit {
 
