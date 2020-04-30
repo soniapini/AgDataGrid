419438fb0663eab838102d00c143fc599a2e2107
soniapini <me@soniapini.info>
2020-04-30 15:45:53 +0200

    build: aggiunti polifyls per IE

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,14 +19,31 @@
  */
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
+import 'classlist.js'; // Run `npm install --save classlist.js`.
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+// import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js'; // Run `npm install --save web-animations-js`.
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+// import 'core-js/es/weak-map';
+// import 'core-js/es/set';
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -51,11 +68,6 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
-
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
